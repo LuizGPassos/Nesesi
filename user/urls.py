c6295004818301862bf3b130a657a785ed730aae
Luiz Passos <lzgustavo.passos@gmail.com>
2023-12-23 18:10:16 -0300

    Pages Navegations

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from user.views import login
+from user.views import login, register
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
